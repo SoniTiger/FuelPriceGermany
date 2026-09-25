4bfce1e3b1a346bb16100bab7dc9ffcfd4468717
Sonsiri <106734463+SoniTiger@users.noreply.github.com>
2026-09-25 15:46:54 +0200

    Cloudflare Worker angebunden

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ async function searchLocation() {
 
         const response =
             await fetch(
-                `/api/geocode?query=${encodeURIComponent(query)}`
+                `https://fuelprice-api.sonsiri-sotai.workers.dev//api/geocode?query=${encodeURIComponent(query)}`
             );
 
 
@@ -218,7 +218,7 @@ async function loadStationsFromCoordinates(
     try {
 
         const url =
-            `/api/stations` +
+            `https://fuelprice-api.sonsiri-sotai.workers.dev//api/stations` +
             `?lat=${encodeURIComponent(lat)}` +
             `&lon=${encodeURIComponent(lon)}` +
             `&radius=${encodeURIComponent(radius)}`;
